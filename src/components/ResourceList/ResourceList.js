1e5fe18cf4f52319612de53deda1eef8d69887ad
cholanmadala <madala.satyanarayana@lge.com>
2019-04-24 18:53:55 +0530

    add useEffect of React hooks

--- src/components/ResourceList/ResourceList.js
+++ src/components/ResourceList/ResourceList.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-class ResourceList extends React.Component{
+const ResourceList = ({resource}) => {
+	const [resources, setResources] = useState([]);
+	const fetchResources = async (resource) => {
+		const response = await axios.get(`http://jsonplaceholder.typicode.com/${resource}`);
+		setResources(response.data)
+	}
+	useEffect(() => {
+		fetchResources(resource);
+	}, [resource]);
 
-render () {
 	return (
-		<div>{this.props.resource}</div>
-	)
-}
-
+		<div>
+			<h3>{resource.toUpperCase()}</h3>
+			<ol>{resources.map((item) => <li key={item.id}> {item.title}</li>)}</ol>
+		</div>
+	);
 }
 
 export default ResourceList;
